0d833926fc40712a60669e7ba66a3abd2508f5e6
rdborn <rdborn1@gmail.com>
2018-02-02 16:09:54 -0800

    Optimizer has GP estimation abilities now.

--- python/pyloon/main.py
+++ python/pyloon/main.py
@@ -1,21 +1,52 @@
 from loonsim import LoonSim
+from optiloon.optiloon import LoonPathPlanner
 import numpy as np
+from pandas import DataFrame
+from matplotlib import pyplot as plt
 
-xdim = 50
-ydim = 50
-zdim = 50
+# Set up simulation parameters
+xdim = 10
+ydim = 10
+zdim = 10
 hz = 20
 duration = 6
 
+# Set up flow field
 LS = LoonSim(xdim=xdim, ydim=ydim, zdim=zdim, Fs=hz)
-LS.field.set_planar_flow(int(np.ceil(zdim/2 + 2)),1,0)
-LS.field.set_planar_flow(int(np.ceil(zdim/2 + 3)),np.sqrt(2),np.pi)
-LS.field.set_planar_flow(int(np.ceil(zdim/2 + 4)),1,0)
+LS.field.set_planar_flow(int(np.ceil(0.0)),3.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(1.0)),7.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(2.0)),12.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(3.0)),16.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(4.0)),20.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(5.0)),15.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(6.0)),17.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(7.0)),11.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(8.0)),5.0,0.0)
+LS.field.set_planar_flow(int(np.ceil(9.0)),0.0,0.0)
 
-for i in range(int(np.floor(duration * hz))):
-	LS.propogate(1) # command an ascent rate of 1 unit/sec
+LPP = LoonPathPlanner(field=LS.field, res=1.2)
+
+z = np.linspace(0,zdim-1,100)
+fx = np.zeros(len(z))
+fx_actual = np.zeros(len(z))
+fx_std = np.zeros(len(z))
+for i in range(len(z)):
+	result = LPP.predict([3.5,3.5,z[i]])
+	mag, angle = LS.field.get_flow(5,5,z[i])
+	fx[i] = result[0]
+	fx_actual[i] = mag * np.cos(angle)
+	fx_std[i] = result[3]
+
+fig = plt.figure()
+plt.plot(z, fx_actual, 'r:')
+plt.plot(z, fx, 'b-')
+plt.fill(np.concatenate([z,z[::-1]]), np.concatenate([fx - 3*fx_std, (fx + 3*fx_std)[::-1]]), alpha=0.5, fc='b', ec='None')
+plt.show()
+
+#for i in range(int(np.floor(duration * hz))):
+	#LS.propogate(1) # command an ascent rate of 1 unit/sec
 	#LS.propogate(0)
 
-print(LS.loon_history)
+#print(LS.loon_history)
 
-LS.plot()
+#LS.plot()
--- python/pyloon/optiloon/optiloon.py
+++ python/pyloon/optiloon/optiloon.py
@@ -1,29 +1,54 @@
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor as GPR
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process.kernels import RBF, Matern, ConstantKernel as C
 
 class LoonPathPlanner:
     def __init__(self, *args, **kwargs):
         field = kwargs.get('field') # wind field object
         res = kwargs.get('res')     # grid resolution in m
-        x = np.linspace(0, field.xdim, field.xdim / res)
-        y = np.linspace(0, field.ydim, field.ydim / res)
-        z = np.linspace(0, field.zdim, field.zdim / res)
-        XYZ = np.meshgrid(x, y, z)
+        self.retrain(field=field, res=res)
 
-        fx = np.zeros(len(x)*len(y)*len(z))
-        fy = np.zeros(len(x)*len(y)*len(z))
-        fz = np.zeros(len(x)*len(y)*len(z))
-        idx = 0
-        for i in range(len(x)):
-            for j in range(len(y)):
-                for k in range(len(z)):
-                    mag, angle = field.get_flow(XYZ[0][i][j][k], XYZ[1][i][j][k], XYZ[2][i][j][k])
-                    fx[idx] = fd * cos(angle)
-            		fy[idx] = fd * sin(angle)
-            		fz[idx] = 0
-                    
+    def retrain(self, *args, **kwargs):
+        field = kwargs.get('field') # wind field object
+        res = kwargs.get('res')     # grid resolution in m
 
+        # Get training points
+        x = np.linspace(0, field.xdim-1, int(np.floor(field.xdim / res)))
+        y = np.linspace(0, field.ydim-1, int(np.floor(field.ydim / res)))
+        z = np.linspace(0, field.zdim-1, int(np.floor(field.zdim / res)))
+        xyz = np.vstack(map(np.ravel, np.meshgrid(x, y, z))).T
 
-        kernel = C(1.0, (1e-3, 1e-3)) * RBF(10, (1e-3, 1e-3))
-        self.GPx = GPR(kernel=kernel, n_restarts_optimizer=9)
+        # Get training observations
+        fx = np.zeros(len(xyz))
+        fy = np.zeros(len(xyz))
+        fz = np.zeros(len(xyz))
+        for i in range(len(xyz)):
+            mag, angle = field.get_flow(xyz[i,0], xyz[i,1], xyz[i,2])
+            fx[i] = mag * np.cos(angle)
+            fy[i] = mag * np.sin(angle)
+            fz[i] = 0
+
+        # Set up our Gaussian Process (yay!)
+        L = 1
+        lo = 1e-3
+        hi = 1e2
+        kernel = C(1.0, (1e-3, 1e3)) * Matern(length_scale=[L, L, L], length_scale_bounds=(lo, hi), nu=1.5)
+        self.GPx = GPR(kernel=kernel, n_restarts_optimizer=3)
+        self.GPy = GPR(kernel=kernel, n_restarts_optimizer=3)
+        self.GPz = GPR(kernel=kernel, n_restarts_optimizer=3)
+
+        # Train our Gaussian Gaussian Process
+        print("Training GPx")
+        self.GPx.fit(xyz, fx.ravel())
+        print("Training GPy")
+        self.GPy.fit(xyz, fy.ravel())
+        print("Training GPz")
+        self.GPz.fit(xyz, fz.ravel())
+
+        return True
+
+    def predict(self, p):
+        mean_fx, std_fx = self.GPx.predict(np.atleast_2d(p), return_std=True)
+        mean_fy, std_fy = self.GPy.predict(np.atleast_2d(p), return_std=True)
+        mean_fz, std_fz = self.GPz.predict(np.atleast_2d(p), return_std=True)
+        return mean_fx, mean_fy, mean_fz, std_fx, std_fy, std_fz
